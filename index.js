4a65a2b9071e146aad463024f333035a3c8c0072
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2016-10-08 17:32:33 +0200

    fix defaults

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 'use strict';
 
 module.exports = function poltocar(r, theta) {
+  if (arguments.length === 1) {
+    theta = r;
+    r = 1;
+  }
+
   var r = parseFloat(r) || 1;
-  var theta = (typeof theta !== 'undefined') ? parseFloat(theta) : r;
+  var theta = parseFloat(theta) || 0;
 
   return {
     x: r * Math.cos(theta),
