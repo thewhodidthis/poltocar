39903a22b7120aca46c4997e8c61c3b2c7947163
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-02-16 04:56:41 +0200

    fix example

--- example/index.js
+++ example/index.js
@@ -3,19 +3,17 @@
 const fs = require('fs');
 const pol2car = require('../');
 
-const toRad = deg => deg * (Math.PI / 180);
-const concat = (acc, val) => acc + val;
-
 const size = 360;
 const halfSize = size * 0.5;
 const radius = size * 0.325;
 const filepath = './example/dial.svg';
 
-const svg = points => {
+const toRad = deg => deg * (Math.PI / 180);
+const toSvg = points => {
   // http://stackoverflow.com/questions/17455436/is-there-a-way-to-convert-json-to-an-svg-object
   const node = point => `<circle cx="${point.x}" cy="${point.y}" r="2" fill="blue"/>`;
+  const body = points.map(node).reduce((acc, val) => acc + val, '');
   const head = `<svg width="${size}px" height="${size}px" version="1.1" xmlns="http://www.w3.org/2000/svg">\n`;
-  const body = points.map(node).reduce(concat, '');
   const foot = `</svg>\n`;
 
   return head + body + foot;
@@ -31,15 +29,11 @@ const createPoint = i => {
   };
 };
 
-const totalPoints = 36;
-const pointStep = 360 / totalPoints;
-const points = [];
+const total = 36;
+const step = 360 / Math.max(total, 1);
+const points = Array.from(Array(total)).map((v, i) => i * step).map(createPoint);
 
-for (let i = 0; i < totalPoints; i += 1) {
-  points.push(createPoint(i * pointStep));
-}
-
-fs.writeFile(filepath, svg(points), (error) => {
+fs.writeFile(filepath, toSvg(points), (error) => {
   if (error) {
     console.error(error);
   } else {
