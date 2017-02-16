b75ae2adc58b7433d655e0f259759478fcc0505a
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-02-16 02:51:41 +0200

    rebuild

--- dist/poltocar.js
+++ dist/poltocar.js
@@ -1,15 +1,14 @@
 var poltocar = (function () {
   'use strict';
 
-  function poltocar() {
-    var theta = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+  var poltocar = function poltocar() {
+    var t = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
     var r = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
-
     return {
-      x: r * Math.cos(theta),
-      y: r * Math.sin(theta)
+      x: r * Math.cos(t),
+      y: r * Math.sin(t)
     };
-  }
+  };
 
   return poltocar;
 
