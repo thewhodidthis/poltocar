7faaf127abcd64a0ad5eb24e4ba80d0c0fd6d584
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-02-28 20:03:19 +0200

    test: add assertion for nonsensical args

--- test/index.js
+++ test/index.js
@@ -1,18 +1,28 @@
 const test = require('tape');
-const poltocar = require('../');
+const pol2car = require('../');
 
 test('return value is of type and contains', (t) => {
   t.plan(2);
 
-  t.equal(typeof poltocar(), 'object');
-  t.deepEqual(poltocar(0), { x: 1, y: 0 });
+  t.equal(typeof pol2car(), 'object');
+  t.deepEqual(pol2car(0), { x: 1, y: 0 });
 });
 
 test('return value compares with known result', (t) => {
-  t.plan(2);
-
   const theta = Math.pow(2, -53);
 
-  t.equals(poltocar(Math.PI / 6).x - (Math.sqrt(3) / 2), theta);
-  t.equals((Math.sqrt(2) / -2) - poltocar(7 * Math.PI / 4).y, theta);
+  t.plan(2);
+  t.equals(pol2car(Math.PI / 6).x - (Math.sqrt(3) / 2), theta);
+  t.equals((Math.sqrt(2) / -2) - pol2car(7 * Math.PI / 4).y, theta);
+});
+
+test('will not throw if misconfigured', (t) => {
+  try {
+    pol2car(undefined, Infinity);
+    t.pass('should not throw');
+  } catch (e) {
+    t.fail();
+  }
+
+  t.end();
 });
