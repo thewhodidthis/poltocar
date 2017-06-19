044d6800e4ec0c425275cc6f30a244a6f74c7d3e
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-06-19 11:19:14 +0200

    module: rebuild per latest rollup config amends

--- dist/poltocar.js
+++ dist/poltocar.js
@@ -1,25 +1,24 @@
 var poltocar = (function () {
-  'use strict';
+'use strict';
 
-  /**
-   * Helps covert from polar
-   * @module poltocar
-   * @param {Number} t - Angle (theta)
-   * @param {Number} r - Radius
-   * @returns {Object} - Vector
-   * @example
-   * poltocar(Math.PI);
-   */
-  var poltocar = function poltocar() {
-    var t = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
-    var r = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
-    return {
-      x: r * Math.cos(t),
-      y: r * Math.sin(t)
-    };
+/**
+ * Helps covert from polar
+ * @module poltocar
+ * @param {Number} t - Angle (theta)
+ * @param {Number} r - Radius
+ * @returns {Object} - Vector
+ * @example
+ * poltocar(Math.PI);
+ */
+var poltocar = function poltocar() {
+  var t = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+  var r = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
+  return {
+    x: r * Math.cos(t),
+    y: r * Math.sin(t)
   };
+};
 
-  return poltocar;
+return poltocar;
 
 }());
-//# sourceMappingURL=poltocar.js.map
