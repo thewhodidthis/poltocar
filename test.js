586c44c5966b44e61ce1a4703905a6d27aee9fbb
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-23 11:59:40 +0100

    test: cleanup

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
 'use strict'
 
 const { equal, ok, deepEqual, fail } = require('tapeless')
-const pol2car = require('./')
+const bender = require('./')
 
-equal(typeof pol2car(), 'object', 'should be equal', 'return value is of type and contains')
-deepEqual(pol2car(0), { x: 1, y: 0 }, 'should be equivalent')
+equal(typeof bender(), 'object', 'should be equal', 'return value is of type and contains')
+deepEqual(bender(0), { x: 1, y: 0 }, 'should be equivalent')
 
 /* eslint no-restricted-properties: [1, { "object": "Math", "property": "pow" }] */
 const expected = Math.pow(2, -53)
-const { x } = pol2car(Math.PI / 6)
+const { x } = bender(Math.PI / 6)
 const t1 = x - (Math.sqrt(3) / 2)
 
 equal(t1, expected, 'should be equal', 'return value compares with known result')
 
-const { y } = pol2car((7 * Math.PI) / 4)
+const { y } = bender((7 * Math.PI) / 4)
 const t2 = (Math.sqrt(2) / -2) - y
 
 equal(t2, expected, 'should be equal')
 
 try {
-  pol2car(undefined, Infinity)
+  bender(undefined, Infinity)
   ok(true, 'should not throw', 'will not throw if misconfigured')
 } catch (e) {
   fail()
