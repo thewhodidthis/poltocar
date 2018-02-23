e1b72296c0afecf3d354c105e334d38e6f34ff3e
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-23 11:59:46 +0100

    example: cleanup

--- example.js
+++ example.js
@@ -1,30 +1,30 @@
 'use strict'
 
+const { exec } = require('child_process')
 const fs = require('fs')
 const os = require('os')
-const pol2car = require('./')
+const bender = require('./')
 
-const { exec } = require('child_process')
+const DEG = Math.PI / 180
 
 const mag = 360
 const mid = mag * 0.5
 
-const ostmpdir = os.tmpdir()
-const filepath = `${ostmpdir}/rose.svg`
-
-const rad = deg => deg * (Math.PI / 180)
-const svg = (points) => {
+const svg = (data) => {
   // http://stackoverflow.com/questions/17455436/is-there-a-way-to-convert-json-to-an-svg-object
-  const node = point => `<circle cx="${point.x}" cy="${point.y}" r="1" fill="white"/>`
+  const node = p => `<circle cx="${p.x}" cy="${p.y}" r="1" fill="white"/>`
+
   const head = `<svg width="${mag}px" height="${mag}px" version="1.1" xmlns="http://www.w3.org/2000/svg">\n`
   const back = `<rect width="${mag}px" height="${mag}px" fill="black"/>`
-  const body = points.map(node).reduce((acc, val) => acc + val, '')
   const foot = '</svg>\n'
+
+  const body = data.map(node).reduce((acc, val) => acc + val, '')
 
   return head + back + body + foot
 }
 
-const pairs = [
+const seed = a => Math.floor(Math.random() * a.length)
+const feed = [
   [2, 3],
   [4, 1],
   [4, 3],
@@ -32,16 +32,14 @@ const pairs = [
   [6, 4]
 ]
 
-const s = Math.floor(Math.random() * pairs.length)
-const [n, d] = pairs[s]
-const k = n / d
+const [n, d] = feed[seed(feed)]
 
 const createPoint = (i) => {
   const space = mid - 2
-  const angle = rad(i)
-  const reach = Math.cos(k * angle)
+  const angle = DEG * i
+  const reach = Math.cos(n / d * angle)
 
-  const { x, y } = pol2car(angle, reach)
+  const { x, y } = bender(angle, reach)
 
   return {
     x: mid + (space * x),
@@ -49,13 +47,14 @@ const createPoint = (i) => {
   }
 }
 
-const points = Array.from(Array(mag * 2 * d)).map((v, i) => i * 0.5).map(createPoint)
+const data = Array.from({ length: mag * d }).map((v, i) => i).map(createPoint)
+const path = `${os.tmpdir()}/rose.svg`
 
-fs.writeFile(filepath, svg(points), (error) => {
-  if (error) {
-    console.error(error)
+fs.writeFile(path, svg(data), (e) => {
+  if (e) {
+    console.error(e)
   } else {
-    exec(`open -a "Google Chrome" ${filepath}`)
+    exec(`open -a "Google Chrome" ${path}`)
   }
 
   process.exit()
