ab256c3fc2e09c5b653c225b574e4c3a94a7c057
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-11-11 16:53:13 +0000

    test: update per tapeling latest

--- test.js
+++ test.js
@@ -3,24 +3,35 @@
 const { equal, ok, deepEqual, fail } = require('tapeless')
 const bender = require('./')
 
-equal(typeof bender(), 'object', 'should be equal', 'return value is of type and contains')
-deepEqual(bender(0), { x: 1, y: 0 }, 'should be equivalent')
+equal
+  .describe('should be equal')
+  .test(typeof bender(), 'object')
+
+deepEqual
+  .describe('should be equivalent', 'return value is of type and contains')
+  .test(bender(0), { x: 1, y: 0 })
 
 /* eslint no-restricted-properties: [1, { "object": "Math", "property": "pow" }] */
 const expected = Math.pow(2, -53)
 const { x } = bender(Math.PI / 6)
 const t1 = x - (Math.sqrt(3) / 2)
 
-equal(t1, expected, 'should be equal', 'return value compares with known result')
+equal
+  .describe('should be equal', 'return value compares with known result')
+  .test(t1, expected)
 
 const { y } = bender((7 * Math.PI) / 4)
 const t2 = (Math.sqrt(2) / -2) - y
 
-equal(t2, expected, 'should be equal')
+equal
+  .describe('should be equal')
+  .test(t2, expected)
 
 try {
   bender(undefined, Infinity)
-  ok(true, 'should not throw', 'will not throw if misconfigured')
+  ok
+    .describe('should not throw', 'will not throw if misconfigured')
+    .test(true)
 } catch (e) {
   fail()
 }
